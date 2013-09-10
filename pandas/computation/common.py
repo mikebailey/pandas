ee0c1babc47abe6c96a4b979de4b6b2f1f447a0e
Phillip Cloud <cpcloud@gmail.com>
2013-09-10 10:59:50 -0400

    CLN/ENH: use display.encoding instead of hard coding

--- pandas/computation/common.py
+++ pandas/computation/common.py
@@ -4,7 +4,7 @@ import numpy as np
 def _ensure_decoded(s):
     """ if we have bytes, decode them to unicode """
     if isinstance(s, (np.bytes_, bytes)):
-        s = s.decode('UTF-8')
+        s = s.decode(pd.get_option('display.encoding'))
     return s
 
 
